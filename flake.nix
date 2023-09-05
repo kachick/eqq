4fcb0617f56579400c5d0ead9a136738eb6d890d
Kenichi Kamiya <kachick1@gmail.com>
2023-09-05 11:27:28 +0900

    Update Nix env

--- flake.nix
+++ flake.nix
@@ -1,26 +1,34 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
-    nixpkgs-ruby.inputs.nixpkgs.follows = "nixpkgs";
+    # Candidate channels
+    #   - https://github.com/kachick/anylang-template/issues/17
+    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
+    # How to update the revision
+    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-ruby, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        ruby = nixpkgs-ruby.lib.packageFromRubyVersionFile {
-          file = ./.ruby-version;
-          inherit system;
-        };
       in
       {
         devShells.default = with pkgs;
           mkShell {
             buildInputs = [
-              ruby
+              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+              bashInteractive
+
+              ruby_3_2
+              # Required to build psych via irb dependency
+              # https://github.com/kachick/irb-power_assert/issues/116
+              # https://github.com/ruby/irb/pull/648
+              libyaml
+
               dprint
+              tree
               nil
               nixpkgs-fmt
               typos
